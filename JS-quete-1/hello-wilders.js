4bec1d4819508492de62b271f651c7bffe0d6252
amaurylebouteux <54020923+amaurylebouteux@users.noreply.github.com>
2019-09-25 21:43:50 +0200

    Add files via upload

---
+++ JS-quete-1/hello-wilders.js
@@ -0,0 +1,9 @@
+/* hello-wilder.js */
+
+// Define some variables
+const title = 'Star Wars Episode VI: Le Retour du Jedi';
+const year = 1983;
+const director = 'Richard Marquand';
+const message = title + ', réalisé par ' + director + ', est sorti en ' + year + '.';
+
+alert (message);
